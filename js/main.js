2957e29c5a4fb5fa1915d7dfa96d30633ef6b34c
Math Bastos <matheusmaaciel2007@gmail.com>
2021-04-22 16:52:52 -0300

    Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    //FIXED HEADER
+    window.onscroll = function( ) {
+    if(window.pageYOffset > 140) {
+    $('#header').addClass("active");} else {
+    $('#header').removeClass("active");}
+    };
+});
